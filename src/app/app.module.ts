8d4356f10b214c3132906fd8516eacc22f3e0bf4
nydal91 <nydal91@hotmail.com>
2016-09-20 14:35:03 +0200

    new file:   database.rules.json 	modified:   package.json 	modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	new file:   src/app/app.routing.ts 	new file:   src/app/dashboard/dashboard.component.css 	new file:   src/app/dashboard/dashboard.component.html 	new file:   src/app/dashboard/dashboard.component.spec.ts 	new file:   src/app/dashboard/dashboard.component.ts 	new file:   src/app/hero-detail/hero-detail.component.css 	new file:   src/app/hero-detail/hero-detail.component.html 	new file:   src/app/hero-detail/hero-detail.component.spec.ts 	new file:   src/app/hero-detail/hero-detail.component.ts 	new file:   src/app/hero-search.service.spec.ts 	new file:   src/app/hero-search.service.ts 	new file:   src/app/hero-search/hero-search.component.css 	new file:   src/app/hero-search/hero-search.component.html 	new file:   src/app/hero-search/hero-search.component.spec.ts         new file:   src/app/hero-search/hero-search.component.ts 	new file:   src/app/hero.service.spec.ts 	new file:   src/app/hero.service.ts 	new file:   src/app/hero.spec.ts 	new file:   src/app/hero.ts 	new file:   src/app/heroes/heroes.component.css 	new file:   src/app/heroes/heroes.component.html 	new file:   src/app/heroes/heroes.component.spec.ts 	new file:   src/app/heroes/heroes.component.ts 	new file:   src/app/in-memory-data.service.ts 	new file:   src/app/rxjs-extensions.ts 	modified:   src/styles.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,47 @@
+
+
+import './rxjs-extensions';  
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { routing } from './app.routing';
+
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService }         from './hero.service';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    routing,
+     InMemoryWebApiModule.forRoot(InMemoryDataService),
     HttpModule
   ],
-  providers: [],
+  providers: [
+  HeroService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,45 @@
+
+
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HeroesComponent }      from './heroes/heroes.component';
+
+import { DashboardComponent }      from './dashboard/dashboard.component';
+
+import {HeroDetailComponent} from './hero-detail/hero-detail.component';
+
+
+
+const appRoutes: Routes = [
+	  {
+		      path: 'heroes',
+		          component: HeroesComponent
+
+
+
+
+	  },
+	 
+	  {
+		    path: '',
+		      redirectTo: '/dashboard',
+		        pathMatch: 'full'
+	  },
+	 
+	  {
+		    path: 'dashboard',
+		      component: DashboardComponent
+	  },
+	  {
+		    path: 'detail/:id',
+		      component: HeroDetailComponent
+	  }
+
+
+
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
+
+
